83f63c319779c079d5ffb5aac866605eb65a94d3
funshro <rafaeltr080@gmail.com>
2025-10-01 22:22:54 -0300

    att 01/10

--- script/11-todo-list.js
+++ script/11-todo-list.js
@@ -1,10 +1,43 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML = '';
+    
+    for(let i = 0; i < todoList.length; i++){
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueData = todoObject.dueData
+        const {name, dueData} = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueData}</div> 
+            <button class="delete-todo-button" onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            ">Delete</button> 
+        `
+        todoListHTML += html;
+    }
+    
+    console.log(todoListHTML);
+    
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueData = dateInputElement.value;
 
-    todoList.push(name);
+    todoList.push({//name: name, dueData: dueData
+        name, 
+        dueData
+    });
 
     inputElement.value = '';
+    renderTodoList();
 }
